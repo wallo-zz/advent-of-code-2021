f4bb7d8c82078eaa0c9eb695f09d70639f7d6ca1
Walewein Noordam <waleweinnoordam@gmail.com>
2021-12-19 13:58:53 +0100

    Add part 2 for day 1, sonar sweep

--- src/day-1/sonar_sweep.py
+++ src/day-1/sonar_sweep.py
@@ -9,3 +9,17 @@ for index in range(1, len(sonar_data) ):
         
 print("There are {} measurements larger than the previous one!".format(deeper))
 
+# Part Two
+compound_sonar_data = [int(a) + int(b) + int(c) for a, b, c in zip ( \
+    [0, 0, *sonar_data], \
+    [0, *sonar_data, 0], \
+    [*sonar_data, 0, 0] \
+)][2:-2]
+
+compound_deeper=0
+
+for index in range(1, len(compound_sonar_data) ):
+    if int(compound_sonar_data[index]) - int(compound_sonar_data[index - 1]) > 0:
+        compound_deeper += 1
+
+print("There are {} compounded measurements larger than the previous one!".format(compound_deeper))
